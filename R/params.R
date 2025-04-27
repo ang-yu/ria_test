d693fd1ab96b22e5b3672f36c9f2b7f43b181e65
Ang Yu <ang_yu@outlook.com>
2025-04-27 16:20:53 -0500

    .

--- R/params.R
+++ R/params.R
@@ -45,6 +45,7 @@ ria_test <- list(
 		c(j = "data_0", k = "data_0", l = "data_0")
 	),
 	randomized = list(
+		c(i = "data_1zp", j = "data_1", k = "data_0", l = "data_0"),
 		c(i = "data_0zp", j = "data_0", k = "data_0", l = "data_0"),
 		c(i = "data_1zp", j = "data_1", k = "data_1", l = "data_1")
 	)
